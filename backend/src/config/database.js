509b19e61b453e35cc36e2b06b224db1f9b4e8be
Rodrigo Norys <otaviomirandabr@gmail.com>
2026-01-06 10:21:16 -0300

    Fuso horário

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,23 +1,31 @@
 require('dotenv').config();
 
-module.exports = {
-  development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.DATABASE_HOST,
-    port: process.env.PORT,
-    dialect: 'mariadb',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-    },
-    dialectOptions: {
-      timezone: 'America/Sao_Paulo',
-    },
-    timezone: 'America/Sao_Paulo',
+const timezone = process.env.NODE_ENV === 'production'
+  ? 'America/Sao_Paulo'
+  : '-03:00';
+
+const config = {
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT || 3306,
+  dialect: 'mariadb',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
   },
+  dialectOptions: {
+    timezone: timezone,
+  },
+  timezone: timezone,
+};
+
+module.exports = {
+  development: config,
+  production: config,
+  test: config,
 };
