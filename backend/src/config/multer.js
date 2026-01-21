9404a14ac23b0611f5f051cbaec58fbc796fff89
Rodrigo Norys <otaviomirandabr@gmail.com>
2026-01-21 12:30:13 -0300

    Leve organização do código no multer.js

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -6,17 +6,16 @@ const random = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
   fileFilter: async (req, file, cb) => {
-    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/webp') {
       return cb(new multer.MulterError('File needs to be PNG or JPG'));
     }
     try {
       const { student_id } = req.params;
-
       if (!student_id) return cb(new multer.MulterError('STUDENT_ID_MISSING'));
 
       const student = await Student.findByPk(student_id);
-
       if (!student) return cb(new multer.MulterError('STUDENT_NOT_FOUND'));
+
       return cb(null, true);
     } catch (e) {
       return cb(new multer.MulterError('DATABASE_ERROR'));
