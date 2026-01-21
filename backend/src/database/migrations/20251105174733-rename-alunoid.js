9d8bc16bfe437274db6959745c1ee3f7d702a926
Rodrigo Norys <otaviomirandabr@gmail.com>
2026-01-21 13:27:05 -0300

    Remoção de uma migration desnecessária

--- backend/src/database/migrations/20251105174733-rename-alunoid.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('sequelize-cli').Migration} */
-export async function up(queryInterface, Sequelize) {
-  return queryInterface.renameColumn('photos', 'aluno_id', 'student_id');
-}
-
-export async function down(queryInterface, Sequelize) {
-  return queryInterface.renameColumn('photos', 'student_id', 'aluno_id');
-}
