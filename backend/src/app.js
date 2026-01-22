f670793db9e1e0025333fefc635e1b4b98f15a92
Rodrigo Norys <otaviomirandabr@gmail.com>
2026-01-22 09:52:42 -0300

    Testando Helmet

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,9 +25,9 @@ class App {
 
   middlewares() {
     this.app.use(cors());
-    this.app.use(helmet({
-      crossOriginResourcePolicy: { policy: "cross-origin" }
-    }));
+    // this.app.use(helmet({
+    //   crossOriginResourcePolicy: { policy: "cross-origin" }
+    // }));
     this.app.use(delay(2000));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
